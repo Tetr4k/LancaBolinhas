7df8046050def6427a840e910b539c823cddf3e4
Gabriel <g2012ribeiro@gmail.com>
2021-03-24 13:11:25 -0300

    Titulo Janela
    -Alterado titulo da janela do jogo
    -Alterações em sources

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,47 @@ PIG_Teclado meuTeclado;     //variável como mapeamento do teclado
 int main( int argc, char* args[] ){
 
     //criando o jogo (aplicação)
-    CriaJogo("Meu Jogo");
+    CriaJogo("Lanca Bolinhas");
+    SetTituloJanela("Carregando . . .");
+    SetTamanhoJanela(720, 1280);
 
     //associando o teclado (basta uma única vez) com a variável meuTeclado
     meuTeclado = GetTeclado();
 
+
+    CarregaBackground("..//sounds//background.mp3");
+
+    int somImpacto = CriaAudio("..//sounds//soundImpacto.mp3",0);
+    SetVolume(somImpacto,25);
+
+    int bola1 = CriaSprite("..//img//bola1.png");
+    CarregaArquivoFramesSprite(bola1,"..//img//bola1.txt");
+    MoveSprite(bola1,0,0);
+    SetDimensoesSprite(bola1,48,48);
+    MudaFrameSprite(bola1,1);
+
+    /*
+    int bola2 = CriaSprite(".//img//bola2.png");
+    CarregaArquivoFramesSprite(bola2,"bola2.txt");
+    MudaFrameSprite(bola2,1);
+
+    int bola3 = CriaSprite(".//img//bola3.png");
+    CarregaArquivoFramesSprite(bola3,"bola3.txt");
+    MudaFrameSprite(bola3,1);
+    */
+
+
+    /*int seta = CriaSprite("..//img//seta.png");
+    MoveSprite(seta,0,0);
+    SetDimensoesSprite(seta, 626, 484);
+    SetAnguloSprite(seta,15);*/
+
     //loop principal do jogo
+
+    PlayBackground();
+
+    SetTituloJanela("Lanca Bolinhas");
+
     while(JogoRodando()){
 
         //pega um evento que tenha ocorrido desde a última passada do loop
@@ -23,6 +58,9 @@ int main( int argc, char* args[] ){
         IniciaDesenho();
 
         //todas as chamadas de desenho devem ser feitas aqui na ordem desejada
+        DesenhaSprite(bola1);
+        //DesenhaSprite(seta);
+        //PlayAudio(somImpacto);
 
         //o frame totalmente pronto será mostrado na tela
         EncerraDesenho();

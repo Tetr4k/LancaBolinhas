5c3c48682f01033c3e06c820b90572a4b11f4a67
Gabriel <g2012ribeiro@gmail.com>
2021-03-25 16:09:21 -0300

    Criação e Ajusta
    Seta agora funciona como deveria
    Criado trajeto da bola

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 PIG_Evento evento;          //evento ser tratadoi a cada pssada do loop principal
 PIG_Teclado meuTeclado;     //variável como mapeamento do teclado
 
-#define COEFICIENTE_PESO 100
+#define C_PESO 100
 #define VELOCIDADE 500
 #define D_ESQUERDA 20
 #define D_BAIXO 100
@@ -13,11 +13,104 @@ struct Objeto{
     int desenho;
 };
 
-struct Fase{
-    int angulo;
+struct Trajeto{
+    int x[32], y[32];
+};
+
+typedef struct{
+    double angulo;
+    Trajeto trajeto;
     struct Objeto bola;
     vector<int> alvos;
-};
+}Fase;
+
+int *criaModeloAlvos(){//funcao para criar modelo dos alvos
+    int *alvos = (int*) malloc(20*sizeof(int));
+    for(int i=0; i<20; i++){
+        alvos[i] = CriaObjeto("..//img//alvo.png", 0);
+        int tam = i*5+20;
+        SetDimensoesObjeto(alvos[i], tam, tam);
+        SetPivoAbsolutoObjeto(alvos[i], tam/2, tam/2);
+        DefineRaioColisaoObjeto(alvos[i], tam/4);
+        DefineTipoColisaoObjeto(alvos[i], PIG_COLISAO_CIRCULAR);
+    }
+    return alvos;
+}
+
+int *criaModeloBolas(){//funcao para criar modelo das bolas
+    int *bolas = (int*) malloc(3*sizeof(int));
+    bolas[0] = CriaAnimacao("..//img//bola1.png", 0);
+    bolas[1] = CriaAnimacao("..//img//bola2.png", 0);
+    bolas[2] = CriaAnimacao("..//img//bola3.png", 0);
+
+    SetDimensoesAnimacao(bolas[0], 48, 48);
+    SetDimensoesAnimacao(bolas[1], 40, 40);
+    SetDimensoesAnimacao(bolas[2], 40, 40);
+
+    SetPivoAbsolutoAnimacao(bolas[0], 24, 24);
+    SetPivoAbsolutoAnimacao(bolas[1], 20, 20);
+    SetPivoAbsolutoAnimacao(bolas[2], 20, 20);
+
+    DefineRaioColisaoAnimacao(bolas[0], 24);
+    DefineRaioColisaoAnimacao(bolas[1], 20);
+    DefineRaioColisaoAnimacao(bolas[2], 20);
+
+    DefineTipoColisaoAnimacao(bolas[0], PIG_COLISAO_CIRCULAR);
+    DefineTipoColisaoAnimacao(bolas[1], PIG_COLISAO_CIRCULAR);
+    DefineTipoColisaoAnimacao(bolas[2], PIG_COLISAO_CIRCULAR);
+
+    CarregaFramesPorLinhaAnimacao(bolas[0], 1, 1, 10);
+    CarregaFramesPorLinhaAnimacao(bolas[1], 1, 1, 10);
+    CarregaFramesPorLinhaAnimacao(bolas[2], 1, 1, 10);
+
+    CriaModoAnimacao(bolas[0], 0, 1);//animação bola parada
+    CriaModoAnimacao(bolas[1], 0, 1);
+    CriaModoAnimacao(bolas[2], 0, 1);
+
+    InsereFrameAnimacao(bolas[0], 0, 1, 0.05);
+    InsereFrameAnimacao(bolas[1], 0, 1, 0.05);
+    InsereFrameAnimacao(bolas[2], 0, 1, 0.05);
+
+    MudaModoAnimacao(bolas[0], 0, 0);
+    MudaModoAnimacao(bolas[1], 0, 0);
+    MudaModoAnimacao(bolas[2], 0, 0);
+
+    CriaModoAnimacao(bolas[0], 1, 1);//animação bola em movimento
+    CriaModoAnimacao(bolas[1], 1, 1);
+    CriaModoAnimacao(bolas[2], 1, 1);
+
+    for(int i=0; i<10; i++){
+        InsereFrameAnimacao(bolas[0], 1, i+1, 0.05);
+        InsereFrameAnimacao(bolas[1], 1, i+1, 0.05);
+        InsereFrameAnimacao(bolas[2], 1, i+1, 0.05);
+    }
+
+    return bolas;
+}
+
+int calculaX(Fase fase, double t){
+    double radiano = fase.angulo*M_PI/180.0;
+    return fase.bola.xi + VELOCIDADE*cos(radiano)*t;
+}
+
+int calculaY(Fase fase, double t){
+    double radiano = fase.angulo*M_PI/180.0;
+    return fase.bola.yi + VELOCIDADE*sin(radiano)*t-fase.bola.g*pow(t, 2)/2;
+}
+
+Trajeto calculaTrajeto(Fase fase){
+    Trajeto aux;
+    int alt, lag;
+    GetDimensoesAnimacao(fase.bola.desenho, &alt, &lag);//pega dimensão da bola para ajustar trajeto
+    for(int i=0; i<32; i++){
+        double radiano = fase.angulo*M_PI/180.0, t;
+        aux.x[i] = 40*i+D_ESQUERDA;
+        t = (aux.x[i]-fase.bola.xi)/(VELOCIDADE*cos(radiano));
+        aux.x[i] +=lag/2;//ajusta x do trajeto
+        aux.y[i] = calculaY(fase, t)+(alt/2);//calcula e ajusta y do trajeto
+    }
+    return aux;
+}
 
 int main( int argc, char* args[] ){
 
@@ -32,71 +125,23 @@ int main( int argc, char* args[] ){
     CarregaBackground("..//sounds//background.mp3");//cria musica de fundo
 
     int somImpacto = CriaAudio("..//sounds//soundImpacto.mp3",0);//cria audio de impacto
-    SetVolume(somImpacto, 1);
+    SetVolume(somImpacto, 50);
 
-    int *alvos = (int*) malloc(20*sizeof(int));//cria modelo dos alvos
-    for(int i=0; i<20; i++){
-        alvos[i] = CriaObjeto("..//img//alvo.png", 0);
-        int tam = i*5+20;
-        SetDimensoesObjeto(alvos[i], tam, tam);
-        SetPivoAbsolutoObjeto(alvos[i], tam/2, tam/2);
-        DefineRaioColisaoObjeto(alvos[i], tam/4);
-        DefineTipoColisaoObjeto(alvos[i], PIG_COLISAO_CIRCULAR);
-    }
+    int *alvos = criaModeloAlvos();
+    int *bolas = criaModeloBolas();
 
-    int *bolas = (int*) malloc(3*sizeof(int));//cria modelo das bolas
-    bolas[0] = CriaAnimacao("..//img//bola1.png", 0);
-    bolas[1] = CriaAnimacao("..//img//bola2.png", 0);
-    bolas[2] = CriaAnimacao("..//img//bola3.png", 0);
-    SetDimensoesAnimacao(bolas[0], 48, 48);
-    SetDimensoesAnimacao(bolas[1], 40, 40);
-    SetDimensoesAnimacao(bolas[2], 40, 40);
-    SetPivoAbsolutoAnimacao(bolas[0], 24, 24);
-    SetPivoAbsolutoAnimacao(bolas[1], 20, 20);
-    SetPivoAbsolutoAnimacao(bolas[2], 20, 20);
-    DefineRaioColisaoAnimacao(bolas[0], 24);
-    DefineRaioColisaoAnimacao(bolas[1], 20);
-    DefineRaioColisaoAnimacao(bolas[2], 20);
-    DefineTipoColisaoAnimacao(bolas[0], PIG_COLISAO_CIRCULAR);
-    DefineTipoColisaoAnimacao(bolas[1], PIG_COLISAO_CIRCULAR);
-    DefineTipoColisaoAnimacao(bolas[2], PIG_COLISAO_CIRCULAR);
-    CarregaFramesPorLinhaAnimacao(bolas[0], 1, 1, 10);
-    CarregaFramesPorLinhaAnimacao(bolas[1], 1, 1, 10);
-    CarregaFramesPorLinhaAnimacao(bolas[2], 1, 1, 10);
-
-    CriaModoAnimacao(bolas[0], 0, 1);//animação bola parada
-    CriaModoAnimacao(bolas[1], 0, 1);
-    CriaModoAnimacao(bolas[2], 0, 1);
-    InsereFrameAnimacao(bolas[0], 0, 1, 0.05);
-    InsereFrameAnimacao(bolas[1], 0, 1, 0.05);
-    InsereFrameAnimacao(bolas[2], 0, 1, 0.05);
-    MudaModoAnimacao(bolas[0], 0, 0);
-    MudaModoAnimacao(bolas[1], 0, 0);
-    MudaModoAnimacao(bolas[2], 0, 0);
-
-    CriaModoAnimacao(bolas[0], 1, 1);//animação bola em movimento
-    CriaModoAnimacao(bolas[1], 1, 1);
-    CriaModoAnimacao(bolas[2], 1, 1);
-    for(int i=0; i<10; i++){
-        InsereFrameAnimacao(bolas[0], 1, i+1, 0.05);
-        InsereFrameAnimacao(bolas[1], 1, i+1, 0.05);
-        InsereFrameAnimacao(bolas[2], 1, i+1, 0.05);
-    }
-
-    int seta = CriaSprite("..//img//seta.png", 0);//cria seta
-    SetDimensoesSprite(seta, 40, 120);
-    SetPivoAbsolutoSprite(seta, 20, 0);
-
-    struct Fase fase;//leitura da fase
-    int posX, posY, tipo;
+    Fase fase;//leitura da fase
     double yInicial;
     FILE *arq = fopen("../fases/fase1.txt", "r");
+    int posX, posY, tipo;
     fscanf(arq, "%d %d", &posY, &tipo);
     fase.bola.desenho = CriaAnimacao(bolas[tipo], 0);
-    fase.bola.g = COEFICIENTE_PESO*(tipo+1);
-    fase.bola.x = fase.bola.xi = D_ESQUERDA;
-    fase.bola.y = fase.bola.yi = posY+D_BAIXO;
-    fase.angulo = 0;
+    fase.bola.g  = C_PESO*(tipo+1);
+    fase.bola.xi = D_ESQUERDA;
+    fase.bola.yi = posY+D_BAIXO;
+    fase.bola.x  = fase.bola.xi;
+    fase.bola.y  = fase.bola.yi;
+    fase.angulo  = 30;
     while(!feof(arq)){
         fscanf(arq, "%d %d %d", &posX, &posY, &tipo);
         int alvo = CriaObjeto(alvos[tipo], 0);
@@ -105,8 +150,11 @@ int main( int argc, char* args[] ){
     }
     fclose(arq);
 
-    MoveSprite(seta, fase.bola.x, fase.bola.y);
-    SetPivoAbsolutoSprite(seta, fase.bola.x, fase.bola.y);
+    GetDimensoesAnimacao(fase.bola.desenho, &posX, &posY);
+    int seta = CriaSprite("..//img//seta.png", 0);//cria seta
+    SetDimensoesSprite(seta, 20, 100);
+    SetPivoAbsolutoSprite(seta, 10, 0);
+    MoveSprite(seta, fase.bola.xi+posX/2-10, fase.bola.yi+posY/2);
 
     //loop principal do jogo
 
@@ -115,7 +163,9 @@ int main( int argc, char* args[] ){
     SetTituloJanela("Lanca Bolinhas");
 
     int timer = CriaTimer(1);
-    double t;
+    double t=0;
+
+    fase.trajeto = calculaTrajeto(fase);
 
     while(JogoRodando()){
 
@@ -125,6 +175,8 @@ int main( int argc, char* args[] ){
         //aqui o evento deve ser tratado e as coisas devem ser atualizadas
 
         for(int i=0; i<fase.alvos.size(); i++) if(TestaColisaoAnimacaoObjeto(fase.bola.desenho, fase.alvos[i])){//teste de colisão
+            DestroiObjeto(fase.alvos[i]);
+            fase.alvos.erase (fase.alvos.begin()+i);
             PlayAudio(somImpacto);
             //quebra e fim de fase;
         }
@@ -135,19 +187,29 @@ int main( int argc, char* args[] ){
                     MudaModoAnimacao(fase.bola.desenho, 1, 0);
             }
 
-            if(evento.teclado.acao == PIG_TECLA_PRESSIONADA){//ajusta angulo usando as setas
-                if(evento.teclado.tecla == PIG_TECLA_CIMA && fase.angulo<89) fase.angulo++;
-                if(evento.teclado.tecla == PIG_TECLA_BAIXO && fase.angulo>-89) fase.angulo--;
+            if(evento.teclado.acao == PIG_TECLA_PRESSIONADA && t==0){//ajusta angulo usando as setas
+                if(evento.teclado.tecla == PIG_TECLA_CIMA && fase.angulo<89){
+                    fase.angulo+=0.2;
+                    fase.trajeto = calculaTrajeto(fase);
+                }
+                if(evento.teclado.tecla == PIG_TECLA_BAIXO && fase.angulo>-89){
+                    fase.angulo-=0.2;
+                    fase.trajeto = calculaTrajeto(fase);
+                }
             }
         }
 
-        t=TempoDecorrido(timer);
-        fase.bola.y = fase.bola.yi + VELOCIDADE*sin(fase.angulo*M_PI/180.0)*t-fase.bola.g*pow(t, 2)/2;//calcula y
-        fase.bola.x = fase.bola.xi + VELOCIDADE*cos(fase.angulo*M_PI/180.0)*t;//calcula x
 
-        MoveAnimacao(fase.bola.desenho, fase.bola.x, fase.bola.y);//atualiza posição do objeto
+        if(fase.bola.y<D_BAIXO/2) {
+            break;//testa altura da bolinha
+        }
+        else{
+            t=TempoDecorrido(timer);
+            fase.bola.y = calculaY(fase, t);////calcula y
+            fase.bola.x = calculaX(fase, t);////calcula x
+            MoveAnimacao(fase.bola.desenho, fase.bola.x, fase.bola.y);//atualiza posição do objeto
+        }
 
-        if(fase.bola.y<0) break;//testa altura da bolinha
 
         //será feita aprepação do frame que será exibido na tela
         IniciaDesenho();
@@ -156,15 +218,16 @@ int main( int argc, char* args[] ){
 
         DesenhaSpriteSimples("..//img//fundo.png", 0, 0, 0);//fundo
 
-        SetAnguloSprite(seta, fase.angulo);//ajusta angulo da seta <---- Não funciona
-        DesenhaSprite(seta);//desenha a seta
-        //DesenhaLinhasSequencia(20, 20, 400, 400, VERDE);
+        if(t==0){
+            DesenhaSprite(seta);//desenha a seta
+            SetAnguloSprite(seta, fase.angulo);//ajusta angulo da seta
+        };
 
-        for(int i=0; i<fase.alvos.size(); i++) DesenhaObjeto(fase.alvos[i]);//alvos
-        DesenhaAnimacao(fase.bola.desenho);//bola
-
-        DesenhaSpriteSimples("..//img//interface.png", 0, 0, 0);//interface
-        EscreveInteiroCentralizado(fase.angulo, 640, 0, AZUL);//escreve o angulo na tela
+        for(int i=0; i<fase.alvos.size(); i++) DesenhaObjeto(fase.alvos[i]);//desenha alvos
+        DesenhaLinhasSequencia(fase.trajeto.x, fase.trajeto.y, 32, AZUL);//desenha trajeto
+        DesenhaAnimacao(fase.bola.desenho);//desenha bola
+        DesenhaSpriteSimples("..//img//interface.png", 0, 0, 0);//desenha fundo da interface
+        EscreveDoubleCentralizado(fase.angulo, 1, 640, 0, AZUL);//escreve o angulo na interface
 
         //Indicar que é angulo
         //Indicar que deve usar as setinhas para aumentar e diminuir
@@ -182,4 +245,3 @@ int main( int argc, char* args[] ){
     free(alvos);
     return 0;
 }
-

270e0d22af03fb172a0672d71c8abede3bb92e64
Gabriel <g2012ribeiro@gmail.com>
2021-03-24 22:39:04 -0300

    Eventos
    O jogo funciona \0/

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@ PIG_Evento evento;          //evento ser tratadoi a cada pssada do loop principa
 PIG_Teclado meuTeclado;     //variável como mapeamento do teclado
 
 struct Objeto{
-    int x, y, desenho;
+    double x, y, xi, yi, g;
+    int desenho;
 };
 
 struct Fase{
     int angulo;
     struct Objeto bola;
-    vector<struct Objeto> alvos;
+    vector<int> alvos;
 };
 
 int main( int argc, char* args[] ){
@@ -23,27 +24,38 @@ int main( int argc, char* args[] ){
     //associando o teclado (basta uma única vez) com a variável meuTeclado
     meuTeclado = GetTeclado();
 
-    //cria audios
-    CarregaBackground("..//sounds//background.mp3");
 
-    int somImpacto = CriaAudio("..//sounds//soundImpacto.mp3",0);
-    SetVolume(somImpacto,25);
+    CarregaBackground("..//sounds//background.mp3");//cria musica de fundo
 
-    //cria modelo dos alvos
-    int *alvos = (int*) malloc(20*sizeof(int));
+    int somImpacto = CriaAudio("..//sounds//soundImpacto.mp3",0);//cria audio de impacto
+    SetVolume(somImpacto, 1);//isso funciona?
+
+    int *alvos = (int*) malloc(20*sizeof(int));//cria modelo dos alvos
     for(int i=0; i<20; i++){
         alvos[i] = CriaObjeto("..//img//alvo.png");
-        SetDimensoesObjeto(alvos[i], i*5+20, i*5+20);
+        int tam = i*5+20;
+        SetDimensoesObjeto(alvos[i], tam, tam);
+        SetPivoAbsolutoObjeto(alvos[i], tam/2, tam/2);
+        DefineRaioColisaoObjeto(alvos[i], tam/4);
+        DefineTipoColisaoObjeto(alvos[i], PIG_COLISAO_CIRCULAR);
     }
 
-    //cria modelo das bolas
-    int *bolas = (int*) malloc(3*sizeof(int));
+    int *bolas = (int*) malloc(3*sizeof(int));//cria modelo das bolas
     bolas[0] = CriaObjeto("..//img//bola1.png");
     bolas[1] = CriaObjeto("..//img//bola2.png");
     bolas[2] = CriaObjeto("..//img//bola3.png");
     SetDimensoesObjeto(bolas[0], 48, 48);
     SetDimensoesObjeto(bolas[1], 40, 40);
     SetDimensoesObjeto(bolas[2], 40, 40);
+    SetPivoAbsolutoObjeto(bolas[0], 24, 24);
+    SetPivoAbsolutoObjeto(bolas[1], 20, 20);
+    SetPivoAbsolutoObjeto(bolas[2], 20, 20);
+    DefineRaioColisaoObjeto(bolas[0], 24);
+    DefineRaioColisaoObjeto(bolas[1], 20);
+    DefineRaioColisaoObjeto(bolas[2], 20);
+    DefineTipoColisaoObjeto(bolas[0], PIG_COLISAO_CIRCULAR);
+    DefineTipoColisaoObjeto(bolas[1], PIG_COLISAO_CIRCULAR);
+    DefineTipoColisaoObjeto(bolas[2], PIG_COLISAO_CIRCULAR);
     CarregaArquivoFramesObjeto(bolas[0], "..//img//bola1.txt");
     CarregaArquivoFramesObjeto(bolas[1], "..//img//bola2.txt");
     CarregaArquivoFramesObjeto(bolas[2], "..//img//bola3.txt");
@@ -51,38 +63,38 @@ int main( int argc, char* args[] ){
     MudaFrameObjeto(bolas[1], 1);
     MudaFrameObjeto(bolas[2], 1);
 
+    int seta = CriaSprite("..//img//seta.png", 0);//cria seta
+    SetDimensoesSprite(seta, 60, 40);
+    SetPivoAbsolutoSprite(seta, 30, 0);
 
-    /*
-    int seta = CriaSprite("..//img//seta.png");
-    MoveSprite(seta,0,0);
-    SetDimensoesSprite(seta, 626, 484);
-    SetAnguloSprite(seta,15);
-    */
-
-    struct Fase fase;
-    fase.angulo = 0;
+    struct Fase fase;//leitura da fase
     int posX, posY, tipo;
+    double yInicial;
     FILE *arq = fopen("../fases/fase1.txt", "r");
     fscanf(arq, "%d %d", &posY, &tipo);
     fase.bola.desenho = CriaObjeto(bolas[tipo]);
-    fase.bola.x = 10;
-    fase.bola.y = posY+100;
-
-    struct Objeto alvo;
+    fase.bola.g = 50*(tipo+1);
+    fase.bola.x = fase.bola.xi = 10;
+    fase.bola.y = fase.bola.yi = posY+100;
+    fase.angulo = 0;
     while(!feof(arq)){
         fscanf(arq, "%d %d %d", &posX, &posY, &tipo);
-        alvo.desenho = CriaObjeto(alvos[tipo]);
-        alvo.x = posX;
-        alvo.y = posY;
+        int alvo = CriaObjeto(alvos[tipo]);
+        MoveObjeto(alvo, posY, posX);
         fase.alvos.push_back(alvo);
     }
     fclose(arq);
 
+    MoveSprite(seta, fase.bola.x, fase.bola.y);
+    SetPivoAbsolutoSprite(seta, fase.bola.x, fase.bola.y);
     //loop principal do jogo
 
-    PlayBackground();
+    //PlayBackground(); <<<<<<<<<<<<<<<<<AD<AD<AD<AD<A<DA<D<AD<AD<AD-----------------------------------------------------
 
     SetTituloJanela("Lanca Bolinhas");
+
+    int timer = CriaTimer(1);
+    double t;
 
     while(JogoRodando()){
 
@@ -91,49 +103,55 @@ int main( int argc, char* args[] ){
 
         //aqui o evento deve ser tratado e as coisas devem ser atualizadas
 
-        if(evento.teclado.tecla == PIG_TECLA_CIMA){
-            //angulo++
+        for(int i=0; i<fase.alvos.size(); i++) if(TestaColisaoObjetos(fase.bola.desenho, fase.alvos[i])){//teste de colisão
+            PlayAudio(somImpacto);
+            //quebra e fim de fase;
         }
-        if(evento.teclado.tecla == PIG_TECLA_BAIXO){
-            //angulo--
+
+        if(evento.tipoEvento == PIG_EVENTO_TECLADO){
+            if(evento.teclado.tecla == PIG_TECLA_BARRAESPACO) DespausaTimer(timer);//verifica se espaço foi apertado para arremessar bolinha
+
+            if(evento.teclado.acao == PIG_TECLA_PRESSIONADA){//ajusta angulo com setinhas
+                if(evento.teclado.tecla == PIG_TECLA_CIMA)
+                    if(fase.angulo<90) fase.angulo++;
+                if(evento.teclado.tecla == PIG_TECLA_BAIXO)
+                    if(fase.angulo>-90) fase.angulo--;
+            }
         }
+
+        t=TempoDecorrido(timer);
+        fase.bola.y = fase.bola.yi + 250*sin(fase.angulo*M_PI/180.0)*t-fase.bola.g*pow(t, 2)/2;//calcula y
+        fase.bola.x = fase.bola.xi + 250*cos(fase.angulo*M_PI/180.0)*t;//calcula x
+
+        MoveObjeto(fase.bola.desenho, fase.bola.x, fase.bola.y);//atualiza posição do objeto
+
+        if(fase.bola.y<0) break;//testa altura da bolinha
 
         //será feita aprepação do frame que será exibido na tela
         IniciaDesenho();
 
         //todas as chamadas de desenho devem ser feitas aqui na ordem desejada
 
-        //fundo
-        DesenhaSpriteSimples("..//img//fundo.png", 0, 0, 0);
-
-        //interface
-        DesenhaSpriteSimples("..//img//interface.png", 0, 0, 0);
-
-        //posicao atual do objeto
-        MoveObjeto(fase.bola.desenho, fase.bola.x, fase.bola.y);
-        DesenhaObjeto(fase.bola.desenho);
+        DesenhaSpriteSimples("..//img//fundo.png", 0, 0, 0);//fundo
+        DesenhaSpriteSimples("..//img//interface.png", 0, 0, 0);//interface
+        DesenhaObjeto(fase.bola.desenho);//bola
+        for(int i=0; i<fase.alvos.size(); i++) DesenhaObjeto(fase.alvos[i]); //alvos
 
 
-        for(int i=0; i<fase.alvos.size(); i++){
-            MoveObjeto(fase.alvos[i].desenho, fase.alvos[i].y, fase.alvos[i].x);
-            DesenhaObjeto(fase.alvos[i].desenho);
-        }
-
-        //DesenhaSprite(seta);
+        SetAnguloSprite(seta, fase.angulo);//ajusta angulo da seta <----Não funciona
+        DesenhaSprite(seta);//desenha a seta
 
         //DesenhaLinhasSequencia(20, 20, 400, 400, VERDE);
-        //PlayAudio(somImpacto);
+
+        EscreveInteiroCentralizado(fase.angulo, 640, 0, AZUL);//escreve o angulo na tela
 
         //o frame totalmente pronto será mostrado na tela
         EncerraDesenho();
     }
-
     //o jogo será encerrado
+    FinalizaJogo();
     free(bolas);
     free(alvos);
-
-    FinalizaJogo();
-
     return 0;
 }
 
